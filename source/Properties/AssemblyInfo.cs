4a018a7aad7b8b3363bd5905af382f684fe7dd67
John Tasler <taz@mysticpop.com>
2023-04-06 16:49:30 -0700

    Update for 1.0.3

--- source/Properties/AssemblyInfo.cs
+++ source/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyVersion("1.0.3")]
 [assembly: AssemblyProduct("Renewed Power Commands for Visual Studio 2022")]
 [assembly: AssemblyCompany("John Tasler")]
